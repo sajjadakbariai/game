f8f482041e38c4c5f4509187238aa16c6f64e5e4
sajjadakbariai <sajjadakbari.ai@gmail.com>
2025-04-26 08:35:34 +0330

    Create DashboardLayout.vue

---
+++ src/layouts/DashboardLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="min-h-screen flex flex-col">
+    <AppHeader />
+    
+    <div class="flex flex-1">
+      <AppSidebar />
+      
+      <main class="flex-1 p-6 overflow-auto">
+        <div class="max-w-7xl mx-auto">
+          <RouterView />
+        </div>
+      </main>
+    </div>
+    
+    <AppFooter />
+  </div>
+</template>
+
+<script setup>
+import AppHeader from '@/components/layout/Header.vue'
+import AppSidebar from '@/components/layout/Sidebar.vue'
+import AppFooter from '@/components/layout/Footer.vue'
+</script>
